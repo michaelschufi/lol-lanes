8e893f9a29ec14e7118b7a6082f82eb9abc6cf0d
Michael Schaufelberger <michael.schufi@gmail.com>
2016-02-09 23:18:45 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+var lanes = [ "0", "1", "2", "3", "4" ]
+
+$( "button" ).click( function( event ) {
+	event.preventDefault()
+	var test = $.get( "https://www.random.org/sequences/?min=0&max=4&col=1&format=plain&rnd=new", function( data ) {
+		var regMatch = data.match( /./g )
+		var laneSequence = regMatch.filter( function( laneId ) {
+			return lanes.indexOf( laneId ) != -1
+		} )
+		
+		$( "#primary" ).attr( "src", "assets/" + laneSequence[0] + ".png");
+		$( "#secondary" ).attr( "src", "assets/" + laneSequence[1] + ".png");
+	} )
+} )
+
+$( ".lanes" ).click( function( event ) {
+	var laneId = event.target.getAttribute( "laneId" )
+	
+	if ( laneId != null ) {
+		if ( lanes.indexOf( laneId ) === -1 ) {
+			lanes.push( laneId )
+			$( event.target ).removeClass( "inactive" )
+		} else {
+			lanes = lanes.filter( function( id ) { 
+				return laneId != id
+			} )
+			$( event.target).addClass( "inactive" )
+		}
+	}
+} )
