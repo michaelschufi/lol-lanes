54f8e6b3d066e50ed49b384ee9c6e1a476647d47
Michael Schaufelberger <michael.schufi@gmail.com>
2016-02-09 23:39:15 +0100

    added error message if < 2 roles are chosen

--- main.js
+++ main.js
@@ -2,16 +2,23 @@ var lanes = [ "0", "1", "2", "3", "4" ]
 
 $( "button" ).click( function( event ) {
 	event.preventDefault()
-	var test = $.get( "https://www.random.org/sequences/?min=0&max=4&col=1&format=plain&rnd=new", function( data ) {
-		var regMatch = data.match( /./g )
-		var laneSequence = regMatch.filter( function( laneId ) {
-			return lanes.indexOf( laneId ) != -1
+
+	if ( lanes.length < 2) {
+		alert( "You have to choose at least 2 lanes!")
+		$( ".result" ).addClass( "hidden" )
+	} else {
+		var test = $.get( "https://www.random.org/sequences/?min=0&max=4&col=1&format=plain&rnd=new", function( data ) {
+			var regMatch = data.match( /./g )
+			var laneSequence = regMatch.filter( function( laneId ) {
+				return lanes.indexOf( laneId ) != -1
+			} )
+			
+			$( "#primary" ).attr( "src", "assets/" + laneSequence[0] + ".png");
+			$( "#secondary" ).attr( "src", "assets/" + laneSequence[1] + ".png");
+			$( ".result" ).removeClass( "hidden" )
 		} )
-		
-		$( "#primary" ).attr( "src", "assets/" + laneSequence[0] + ".png");
-		$( "#secondary" ).attr( "src", "assets/" + laneSequence[1] + ".png");
-		$( ".result" ).removeClass( "hidden" )
-	} )
+	}
+
 } )
 
 $( ".lanes" ).click( function( event ) {
